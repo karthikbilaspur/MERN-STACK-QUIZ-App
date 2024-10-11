d771a3f519db40336410098606446ce5a926e1d1
V. Karthik <karthikv81291@gmail.com>
2024-10-11 17:20:11 +0530

    Add files via upload

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,1 +1,12 @@
+//database/conn.js
 
+import mongoose from "mongoose";
+
+export default async function connect(){
+    try {
+        await mongoose.connect(process.env.ATLAS_URI);
+        console.log("Database Connected");
+    } catch (error) {
+        console.log("Invalid Database Connection");
+    }
+}
