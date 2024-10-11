37402c2df22a57f82eaac505e840b290f17dc5c9
V. Karthik <karthikv81291@gmail.com>
2024-10-11 17:00:59 +0530

    Add files via upload

--- client/redux/actions/questionActions.js
+++ client/redux/actions/questionActions.js
@@ -1,1 +1,13 @@
+import { FETCH_QUESTIONS } from './types';
 
+export const fetchQuestions = () => async (dispatch) => {
+  try {
+    const response = await api.get('/questions');
+    dispatch({
+      type: FETCH_QUESTIONS,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
